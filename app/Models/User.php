9e1860ace5ba4894c608287c3f4e2f1c3caccbb4
M2 <italiyankin@gmail.com>
2024-02-12 20:54:46 +0000

    Prepare friends, and role

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -42,4 +43,15 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function friends()
+    {
+        return $this->belongsToMany(User::class, 'friend_user', 'user_id', 'friend_id')
+            ->withTimestamps();
+    }
+    public function friendOf()
+    {
+        return $this->belongsToMany(User::class, 'friend_user', 'friend_id', 'user_id')
+            ->withTimestamps();
+    }
 }
