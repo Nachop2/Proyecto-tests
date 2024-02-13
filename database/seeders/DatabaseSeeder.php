08251002c2cb4871b1564ea6848eb888e19126fe
M2 <italiyankin@gmail.com>
2024-02-13 21:34:34 +0000

    add category seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Category;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,6 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $category = new Category();
+        $category->name = "English";
+        $category->save();
+        $category = new Category();
+        $category->name = "History";
+        $category->save();
+        $category = new Category();
+        $category->name = "Math";
+        $category->save();
+        $category = new Category();
+        $category->name = "Science";
+        $category->save();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
