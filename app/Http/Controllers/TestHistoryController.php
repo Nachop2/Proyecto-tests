668715440370a4f43adc74e852f3576cb756be8b
M2 <italiyankin@gmail.com>
2024-02-26 15:21:32 +0000

    add score and api to update such score

--- app/Http/Controllers/TestHistoryController.php
+++ app/Http/Controllers/TestHistoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\TestHistory;
 class TestHistoryController extends Controller
 {
     public function index()
@@ -20,11 +20,29 @@ class TestHistoryController extends Controller
                     'name' => $testHistory->test->name,
                     'author' => $testHistory->test->user->name, // Adjust based on your author relationship
                     'id' => $testHistory->test->id,
+                    'score' => $testHistory->max_score,
                     'category_names' => $testHistory->test->categories->pluck('name'),
                     'played_at' => $testHistory->played_at,
                 ];
             });
 
         return response()->json($testHistories);
+    }
+
+    public function updateMaxScore(Request $request, $testHistoryId)
+    {
+        $request->validate([
+            'grade' => 'required|numeric',
+        ]);
+
+        $testHistory = TestHistory::findOrFail($testHistoryId);
+
+        if ($request->grade > $testHistory->max_score) {
+            $testHistory->update(['max_score' => $request->grade]);
+            $testHistory->save();
+            return response()->json(['message' => 'Max score updated successfully']);
+        }
+
+        return response()->json(['message' => 'Max score not updated. New score is not higher.'], 422);
     }
 }
